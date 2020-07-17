dc6fb94e31110678c6abca697031a093f6e9c4fa
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-17 19:00:20 -0400

    Added images to be processed by React-Native in one go

---
+++ app/ts/CIT-Modules/ImageTree.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+/**
+ * @providesModule ImageTree
+ * @flow
+ */
+
+const images = {
+  welcomeBackground: require('../../assets/images/HomeScreen/Welcome_Screen_Background.png'),
+  logoBackground: require('../../assets/images/HomeScreen/Welcome_Screen_LOGO.png'),
+
+};
+
+export default images;
