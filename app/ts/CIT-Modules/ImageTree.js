d9823abb567e1d01ee8a2f4a160f7fea37b2150c
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-19 05:15:28 -0400

    Updated the static images

--- app/ts/CIT-Modules/ImageTree.js
+++ app/ts/CIT-Modules/ImageTree.js
@@ -5,12 +5,14 @@
  */
 
 const images = {
-  welcomeBackground: require('../../assets/images/HomeScreen/Welcome_Screen_Background.png'),
-  logoBackground: require('../../assets/images/HomeScreen/Welcome_Screen_LOGO.png'),
+  welcomeBackground: require('../../assets/images/WelcomeScreen/Welcome_Screen_Background.png'),
+  logoBackground: require('../../assets/images/WelcomeScreen/Welcome_Screen_LOGO.png'),
   login_google_logo: require('../../assets/images/LoginScreen/google-logo.png'),
   login_email_logo: require('../../assets/images/LoginScreen/email-logo.png'),
   login_facebook_logo: require('../../assets/images/LoginScreen/facebook-logo.png'),
   login_background: require('../../assets/images/LoginScreen/loginScreen-background.png'),
+  default_profile_picture: require('../../assets/images/RegistrationScreen/defaultProfilePicture.png'),
+  registration_background: require('../../assets/images/RegistrationScreen/Registration_Screen_background.png'),
 };
 
 export default images;
