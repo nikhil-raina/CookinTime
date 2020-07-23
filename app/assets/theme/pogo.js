3f268de731e3c4548d4d41cd627c40eed2e84160
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-23 07:07:45 -0400

    Added global styling file

---
+++ app/assets/theme/pogo.js
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+/**
+ * @providesModule pogo
+ * @flow
+ */
+
+const pogoTheme = {
+
+  LIGHT_GREY: {
+    color: '#797F8C',
+  },
+
+  DARK_GREY: {
+    color: '#454E59',
+  },
+
+  YELLOW: {
+    color: '#F2D750',
+  },
+
+  LIGHT_YELLOW: {
+    color: '#F2E6C2',
+  },
+
+  WHITE: {
+    color: '#F2F2F2',
+  },
+};
+
+export default pogoTheme;
