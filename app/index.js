b99b1e8c8fe2540db3ec3bf5e1f4f640bb41d9e6
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-23 07:08:26 -0400

    Made Registration Screen as the starting point.

--- app/index.js
+++ app/index.js
@@ -6,7 +6,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import LoginScreen from './ts/LoginScreen/LoginScreen';
+import RegistrationScreen from './ts/RegistrationScreen/RegistrationScreen';
 import WelcomeScreen from './ts/WelcomeScreen/WelcomeScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => WelcomeScreen);
+AppRegistry.registerComponent(appName, () => RegistrationScreen);
