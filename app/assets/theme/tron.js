bdc08c2df7b6400e2cf083e176d88e3ccd7872f9
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-23 07:07:58 -0400

    Added global color file

---
+++ app/assets/theme/tron.js
@@ -0,0 +1,70 @@
+/* eslint-disable prettier/prettier */
+/**
+ * @providesModule tron
+ * @flow
+ */
+import {
+  Dimensions,
+} from 'react-native';
+
+const tronTheme = {
+  defaultBackgroundImage: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    backgroundColor: 'rgba(0,0,0,0.6)',
+  },
+
+  font: {
+    medium: {
+      fontFamily: 'JosefinSans-Medium',
+    },
+
+    light: {
+      fontFamily: 'JosefinSans-Light',
+    },
+
+    reg: {
+      fontFamily: 'JosefinSans-Regular',
+    },
+
+    semiBold: {
+      fontFamily: 'JosefinSans-SemiBold',
+    },
+  },
+
+  text: {
+    mainHeader: {
+      fontSize: 36,
+    },
+
+    secondaryHeader: {
+      fontSize: 32,
+    },
+
+    ternaryHeader: {
+      fontSize: 26,
+    },
+
+    subHeader: {
+      fontSize: 20,
+    },
+
+    subHeaderDetail: {
+      fontSize: 18,
+    },
+
+    primaryText: {
+      fontSize: 14,
+    },
+
+    secondaryText: {
+      fontSize: 12,
+    },
+
+    logoHeader: {
+      fontSize: 10,
+    },
+  },
+};
+
+export default tronTheme;
