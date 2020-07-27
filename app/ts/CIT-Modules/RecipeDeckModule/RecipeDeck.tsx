d9be50af01b3845c8b62dc9b6c9fd5e92a7c3e7a
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-27 03:52:30 -0400

    Created the Recipe Deck Module with styling file

---
+++ app/ts/CIT-Modules/RecipeDeckModule/RecipeDeck.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+/**
+ * @providesModule  RecipeDeck
+ * @flow
+ */
+
+import React from 'react';
+import RecipeCard from '../RecipeCardModule/RecipeCard';
+import style from './RecipeDeckStyle';
+
+interface Props {
+  cards: Array<RecipeCard>,
+  title: String,
+}
+
+interface State {
+  cards: Array<RecipeCard>,
+}
+
+export default class RecipeDeck extends React.Component<Props, State> {
+
+  constructor(props: any){
+    super(props);
+  }
+
+}
---
+++ app/ts/CIT-Modules/RecipeDeckModule/RecipeDeckStyle.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+/**
+ * @provideModule RecipeDeckStyle
+ * @flow
+ */
+
+import pogoTheme  from '../../assets/theme/pogo';
+import tronTheme from '../../assets/theme/tron';
+import {
+  StyleSheet,
+} from 'react-native';
+
+
+export default StyleSheet.create({
+  
+});
