bae988819d1b7855f451e5afd4dc0027ced8038a
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-27 23:07:35 -0400

    Made the recipe Cards with appropriate styling

--- app/ts/CIT-Modules/RecipeCardModule/RecipeCard.tsx
+++ app/ts/CIT-Modules/RecipeCardModule/RecipeCard.tsx
@@ -6,10 +6,12 @@
 
 import React from 'react';
 import CITRecipe from '../Objects/CITRecipe';
-import style from './RecipeCardStyle';
+import styles from './RecipeCardStyle';
+import images from '../ImageTree';
 import {
   View,
   Text,
+  Image,
 } from 'react-native';
 
 interface Props {
@@ -27,11 +29,35 @@ export default class RecipeCard extends React.Component<Props, State> {
   }
 
   render(): React.ReactElement {
-    return(
-      <View>
-        <Text>
-          {'Hello'}
-        </Text>
+    const image = images.fried;
+    const saveIcon = images.MAIN_SAVE_ICON;
+    return (
+      <View style={styles.outerContainer}>
+        <View style={styles.innerContainer}>
+          <View style={styles.contentContainer}>
+            <Image  source={image}
+                    style={styles.imageContainer}/>
+          </View>
+          <View style={styles.contentDetailContainer}>
+            <View style={styles.detailContainer}>
+              <View>
+                <Text style={styles.headerTextStyle}>
+                  {'Wings and Fries with Onion Rings'}
+                  {/* TODO: Currently has a 40 character limit. Need to find a way to 
+                  make sure that the text shrinks if it exceeds the limit. */}
+                </Text>
+              </View>
+              <Text style={styles.timeTextStyle}>
+                {'30 - 40 min'}
+              </Text>
+            </View>
+            <View style={styles.saveIconContainer}>
+              {/* TODO: Make this clickable to bring in the Save feature for the account */}
+              <Image  source={saveIcon}
+                      style={styles.saveIconStyle}/>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
--- app/ts/CIT-Modules/RecipeCardModule/RecipeCardStyle.ts
+++ app/ts/CIT-Modules/RecipeCardModule/RecipeCardStyle.ts
@@ -4,13 +4,73 @@
  * @flow
  */
 
-import pogoTheme  from '../../assets/theme/pogo';
-import tronTheme from '../../assets/theme/tron';
+import pogoTheme  from '../../../assets/theme/pogo';
+import tronTheme from '../../../assets/theme/tron';
 import {
   StyleSheet,
 } from 'react-native';
 
 
 export default StyleSheet.create({
-  
+
+  outerContainer: {
+    alignItems: 'center',
+  },
+
+  innerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    elevation: 20,
+  },
+
+  contentContainer: {
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+
+  headerTextStyle: {
+    ...tronTheme.font.semiBold,
+    ...tronTheme.text.primaryText,
+    flexShrink: 1,
+  },
+
+  timeTextStyle: {
+    ...tronTheme.font.light,
+    ...tronTheme.text.secondaryText,
+  },
+
+  contentDetailContainer: {
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    backgroundColor: pogoTheme.WHITE.color,
+    width: 320,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+  },
+
+  imageContainer: {
+    width: 320,
+    height: 200,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden',
+  },
+
+  saveIconStyle: {
+    height: 20,
+    width: 30,
+  },
+
+  saveIconContainer: {
+    justifyContent: 'center',
+  },
+
+  detailContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+
 });
