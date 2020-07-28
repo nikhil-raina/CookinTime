988f42ec6a2c30638ff10a905e60449a8519801d
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-28 14:33:18 -0400

    Added a bunch of TODOs

--- app/ts/MainScreen/MainScreen.tsx
+++ app/ts/MainScreen/MainScreen.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
 /**
  * @providesModule  MainScreen
  * @flow
@@ -51,10 +51,12 @@ export default class MainScreen extends React.Component<Props, State> {
           </Text>
         </View>
         <View style={styles.searchGlassIconContainer}>
+          {/* TODO: Make this clickable that would then show the search screen */}
           <Image  source={searchIcon}
                   style={[styles.imageStyleSecondary, {height: 20}]}/>
         </View>
         <View style={styles.settingsIconConatainer}>
+          {/* TODO: Make this clickable that would then show the settings screen */}
           <Image  source={settingsIcon}
                   style={styles.imageStyleSecondary}/>
         </View>
@@ -67,6 +69,7 @@ export default class MainScreen extends React.Component<Props, State> {
     return (
       <View style={styles.footerOuterContainer}>
         <View style={styles.footerInnerContainer}>
+          {/* TODO: Make this clickable that would navigate to the cook book screen */}
           <Image  source={cookBookIcon}
                   style={styles.imageStyleFooter}/>
         </View>
@@ -79,7 +82,11 @@ export default class MainScreen extends React.Component<Props, State> {
     return (
       <View style={styles.container}>
         {this._renderHeader()}
+        {/* TODO: Add the scrolling function here. Make sure that at least 8 to 12
+        decks would be available to view for the user */}
         <ScrollView>
+          {/* TODO: Implement the recipe Deck here.
+          Would need to work on the shadowing it goes about giving. */}
           <RecipeCard recipe={recipe}/>
         </ScrollView>
         {this._renderFooter()}
