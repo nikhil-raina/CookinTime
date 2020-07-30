ce75950b7b38a390d78050c43c739349796d40ea
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-30 05:04:01 -0400

    Fixed the styling to allow a better way for shrinking the things inside it

--- app/ts/CIT-Modules/RecipeCardModule/RecipeCard.tsx
+++ app/ts/CIT-Modules/RecipeCardModule/RecipeCard.tsx
@@ -34,21 +34,20 @@ export default class RecipeCard extends React.Component<Props, State> {
     const cookingTimeText = this.props.recipe.cooking_time + ' - ' +
                             (this.props.recipe.cooking_time + 10) + ' min';
     return (
-      <View style={styles.outerContainer}>
-        <View style={styles.innerContainer}>
+        <View style={styles.container}>
           <View style={styles.contentContainer}>
             <Image  source={image}
                     style={styles.imageContainer}/>
           </View>
           <View style={styles.contentDetailContainer}>
             <View style={styles.detailContainer}>
-              <View>
+              {/* <View> */}
                 <Text style={styles.headerTextStyle}>
                   {this.props.recipe.recipe_title}
                   {/* TODO: Currently has a 40 character limit. Need to find a way to
                   make sure that the text shrinks if it exceeds the limit. */}
                 </Text>
-              </View>
+              {/* </View> */}
               <Text style={styles.timeTextStyle}>
                 {cookingTimeText}
               </Text>
@@ -59,7 +58,6 @@ export default class RecipeCard extends React.Component<Props, State> {
                       style={styles.saveIconStyle}/>
             </View>
           </View>
-        </View>
       </View>
     );
   }
