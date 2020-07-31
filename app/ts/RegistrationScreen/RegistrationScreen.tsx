a09e6c109dfb8faacd89b83f700bc1662e4c446f
Nikhil Raina <nxr5013@g.rit.edu>
2020-07-30 22:12:16 -0400

    Added the first screen verification

--- app/ts/RegistrationScreen/RegistrationScreen.tsx
+++ app/ts/RegistrationScreen/RegistrationScreen.tsx
@@ -17,19 +17,89 @@ import {
   Text,
   ImageBackground,
   TextInput,
+  TouchableOpacity,
+  Keyboard,
 } from 'react-native';
 
 interface State {
-  showScreen: boolean
+  showFirstScreen: boolean,
+  email: string,
+  password: string,
+  passwordAgain: string,
 }
 
 export default class RegistrationScreen extends React.Component<{}, State> {
 
+  // TODO: Get the placeholder HEX code for all colors
   constructor(props: any) {
     super(props);
     this.state = {
-      showScreen: true,
+      showFirstScreen: true,
+      email: '',
+      password: '',
+      passwordAgain: '',
     };
+  }
+
+  _setEmail(value: string) {
+    this.setState({
+      email: value,
+    });
+  }
+
+  _setPassword(value: string) {
+    this.setState({
+      password: value,
+    });
+  }
+
+  _setPasswordAgain(text: string) {
+    this.setState({
+      passwordAgain: text,
+    });
+  }
+
+  _firstCheckScreen = () => {
+    // TODO: Need to create modals here to show that something was invalid.
+    if (!this._isEmailValid()) {
+      console.log('Email Not Valid');
+      setTimeout(() => {
+        Keyboard.dismiss();
+        this.setState({
+          email: '',
+          password: '',
+          passwordAgain: '',
+        });
+      }, 150);  // The delay is for it to look realistically smooth
+    } else if (!this._isPasswordSame()) {
+      console.log('Password is not the same');
+      setTimeout(() => {
+        Keyboard.dismiss();
+        this.setState({
+          password: '',
+          passwordAgain: '',
+        });
+      }, 150);  // The delay is for it to look realistically smooth
+    } else {
+      console.log('All is Valid');
+      this.setState({
+        showFirstScreen: false,
+      });
+    }
+  }
+
+  _isUsernameValid() {
+    // eslint-disable-next-line no-useless-escape
+    return !/[~` !#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(this.state.email);
+  }
+
+  _isEmailValid() {
+    const expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return expression.test(this.state.email.toLowerCase());
+  }
+
+  _isPasswordSame() {
+    return this.state.password.toLowerCase() === this.state.passwordAgain.toLowerCase();
   }
 
   _renderFirstScreen(): React.ReactElement {
@@ -53,7 +123,10 @@ export default class RegistrationScreen extends React.Component<{}, State> {
                           style={styles.textStyle}
                           autoCompleteType={'email'}
                           underlineColorAndroid={pogoTheme.LIGHT_YELLOW.color}
-                          value={'Nikhil Raina'}/>
+                          onChangeText={(value) => this._setEmail(value)}
+                          placeholder={'Ex. Bob Bobby'}
+                          value={this.state.email}
+                          placeholderTextColor={'white'}/>
             </View>
           </View>
           <View style={styles.nameContainer}>
@@ -65,7 +138,10 @@ export default class RegistrationScreen extends React.Component<{}, State> {
                           secureTextEntry={true}
                           style={styles.textStyle}
                           underlineColorAndroid={pogoTheme.LIGHT_YELLOW.color}
-                          value={'password'}/>
+                          onChangeText={(value) => this._setPassword(value)}
+                          value={this.state.password}
+                          placeholder={'Be secretive'}
+                          placeholderTextColor={'white'}/>
             </View>
           </View>
           <View>
@@ -77,14 +153,19 @@ export default class RegistrationScreen extends React.Component<{}, State> {
                           secureTextEntry={true}
                           style={styles.textStyle}
                           underlineColorAndroid={pogoTheme.LIGHT_YELLOW.color}
-                          value={'password'}/>
+                          onChangeText={(value) => this._setPasswordAgain(value)}
+                          value={this.state.passwordAgain}
+                          placeholder={'Be secretive again'}
+                          placeholderTextColor={'white'}/>
             </View>
           </View>
         </View>
         <View>
-          <Text style={[styles.textStyle, styles.buttonTextStyle]}>
-            {'NEXT'}
-          </Text>
+          <TouchableOpacity onPress={this._firstCheckScreen}>
+            <Text style={[styles.textStyle, styles.buttonTextStyle]}>
+              {'NEXT'}
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -143,9 +224,9 @@ export default class RegistrationScreen extends React.Component<{}, State> {
     return (
       <ImageBackground  source={background_image}
                         style={styles.backgroundImage}>
-        {this.state.showScreen ?
-          this._renderSecondScreen() :
-          this._renderFirstScreen()}
+        {this.state.showFirstScreen ?
+          this._renderFirstScreen() :
+          this._renderSecondScreen()}
       </ImageBackground>
     );
   }
